6427aab60d207adf8b61d19bae68cdbf49ffa39c
Lily Yu <py561@nyu.edu>
2024-04-08 17:56:02 -0400

    different approaches of native css styling in react

---
+++ 03-css-styles-diff-approaches/src/App.jsx
@@ -0,0 +1,15 @@
+// DIFFERENT CSS STYLING APPROACHES IN REACT
+// -----------------------------------------
+// 1. EXTERNAL
+// 2. MODULES
+// 3. INLINE
+
+import Button from "./Button/Button.jsx"
+
+function App() {
+  return (
+    <Button></Button>
+  );
+}
+
+export default App
