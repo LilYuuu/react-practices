fd579ab9e18908ed55b6b91512eb7972dff8f9c1
Lily Yu <py561@nyu.edu>
2024-07-14 17:34:37 -0400

    finished star rating component as api

--- jonas-usepopcorn/src/index.js
+++ jonas-usepopcorn/src/index.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" onSetRating={setMovieRating} />
+      <p>This movie was rate {movieRating} stars.</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
 
-    <StarRating />
+    <StarRating
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      className="test"
+      defaultRating={3}
+    />
+
+    <Test />
   </React.StrictMode>
 );
