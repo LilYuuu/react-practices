cf6752df34c7bb289b24dff3e17d84925b6dd60d
Lily Yu <py561@nyu.edu>
2024-10-02 16:01:57 -0400

    added feature: setting map position with geolocation

--- jonas-worldwise/src/components/Map.jsx
+++ jonas-worldwise/src/components/Map.jsx
@@ -10,11 +10,19 @@ import {
 import styles from "./Map.module.css";
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
 
 function Map() {
+  const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-
   const [searchParams] = useSearchParams();
+  const {
+    isLoading: isLoadingGeolocation,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
+
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
@@ -27,10 +35,22 @@ function Map() {
     [mapLat, mapLng]
   );
 
-  const { cities } = useCities();
+  useEffect(
+    function () {
+      if (geolocationPosition) {
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+      }
+    },
+    [geolocationPosition]
+  );
 
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingGeolocation ? "LOADING..." : "USE YOUR LOCATION"}
+        </Button>
+      )}
       <MapContainer
         className={styles.map}
         center={mapPosition}
