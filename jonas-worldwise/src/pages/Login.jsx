6f16ef1e7a7923667c519dff61ac38825a536cc1
Lily Yu <py561@nyu.edu>
2024-11-19 21:05:27 -0500

    implemented login and logout

--- jonas-worldwise/src/pages/Login.jsx
+++ jonas-worldwise/src/pages/Login.jsx
@@ -1,11 +1,34 @@
+import { useNavigate } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/FakeAuthContext";
 import styles from "./Login.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Button from "../components/Button";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+
+  const { isAuthenticated, login } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (email && password) {
+      login(email, password);
+    }
+  }
+
+  useEffect(
+    function () {
+      if (isAuthenticated === true) {
+        navigate("/app", { replace: true });
+      }
+    },
+    [isAuthenticated, navigate]
+  );
 
   return (
     <main className={styles.login}>
@@ -33,7 +56,9 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary" onClick={handleSubmit}>
+            Login
+          </Button>
         </div>
       </form>
     </main>
